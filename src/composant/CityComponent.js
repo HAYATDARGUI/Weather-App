13a89ae100bea71756b233f0aa1bc88e147dfd1a
HAYAT DARGUI <hayatdargui72@gmail.com>
2022-11-13 21:16:37 +0100

    first commit

---
+++ src/composant/CityComponent.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import React from "react";
+
+const SearchBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin: 20px;
+  border: #A2D9CE solid 2px;
+  border-radius: 5px;
+  & input {
+    padding: 10px;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+  & button {
+    background-color:#A2D9CE;
+    font-size: 14px;
+    padding: 0 10px;
+    color: white;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+`;
+const ChooseCityLabel = styled.span`
+  color:#A2D9CE;
+  margin: 10px auto;
+  font-size: 20px;
+  font-weight: bold;
+`;
+const WelcomeWeatherLogo = styled.img`
+  width: 140px;
+  height: 140px;
+  margin: 40px auto;
+  border-radius: 5px;
+  background:#A2D9CE
+`;
+const CityComponent = (props) => {
+  const { updateCity, fetchWeather } = props;
+  return (
+    <>
+      <WelcomeWeatherLogo src={"/react-weather-app/icons/perfect-day.svg"} />
+      <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
+      <SearchBox onSubmit={fetchWeather}>
+        <input
+          onChange={(e) => updateCity(e.target.value)}
+          placeholder="City"
+        />
+        <button type={"submit"}>Search</button>
+      </SearchBox>
+    </>
+  );
+};
+export default CityComponent;
